9a7a2fcaddd5fc32c2d29dddde160ff90563ecd1
nano.son <nano.son@kakaocorp.com>
2018-03-11 16:22:38 +0900

    [add] stream deepenConcept examples

--- src/main/java/study/stream/DeepenConcept.java
+++ src/main/java/study/stream/DeepenConcept.java
@@ -3,7 +3,10 @@ package study.stream;
 import pojo.Person;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -23,7 +26,11 @@ public class DeepenConcept {
 //        test1();
 //        test2();
 //        test3();
-        test4();
+//        test4();
+//        test5();
+//        test6();
+//        test7();
+        test10();
     }
 
     //Collect연산
@@ -59,6 +66,7 @@ public class DeepenConcept {
         System.out.println("statistics = " + statistics);
     }
 
+    //
     static void test4() {
         String phrase = persons
                 .stream()
@@ -70,7 +78,104 @@ public class DeepenConcept {
     }
 
     //custom map
+    //Collector를 만들기 위해선, 반드시 supplier, accumulator, combiner, finisher를 컬렉션의 네 가지 재료 넘겨줘야 한다.
     static void test5() {
+        Collector<Person, StringJoiner, String> personNameCollector =
+                Collector.of(
+                        () -> new StringJoiner(" | "),          // supplier
+                        (j, p) -> j.add(p.getName().toUpperCase()),  // accumulator
+                        (j1, j2) -> j1.merge(j2),               // combiner
+                        StringJoiner::toString);                // finisher
 
+        String result = persons.stream().collect(personNameCollector);
+        System.out.println("result:"+result);
+    }
+
+    //감소 연산은 스트림의 모든 원소들을 하나의 결과로 합친다. Java 8 는 세 가지 다른 reduce 방법을 제공한다.
+    //1. reduce(BinaryOperator<T> binaryOperator)
+    //2. reduce(Objecd
+    static void test6() {
+        System.out.println("DeepenConcept.test6");
+        System.out.println("가장 나이 많은 사람");
+        persons.stream()
+                .reduce((p1,p2)-> p1.getAge()<p2.getAge()?p2:p1 )
+                .ifPresent(System.out::println);
+
+        System.out.println("가장 나이 적은 사람");
+        persons.stream()
+                .reduce((p1,p2)->p1.getAge()<p2.getAge() ? p2:p1)
+                .ifPresent(System.out::println);
+    }
+
+    //reduce 두 번째 방법
+    //항등값과 BinaryOperator accumulator를 받는다.
+    //이 메서드는 스트림에 있는 모든 사람들의 이름을 이어붙인 문자열과 나이를 전부 더한 값을 가지는 Person을 생성하는데 활용할 수 있다.
+    static void test7() {
+        System.out.println("DeepenConcept.test7");
+        Person result = persons.stream()
+                .reduce(new Person("test",0), (p1,p2)->{
+                    p1.setName(p1.getName()+p2.getName());
+                    p1.setAge(p1.getAge()+p2.getAge());
+                    return p1;
+                });
+        System.out.println("result name:"+result.getName()+", result age:"+result.getAge());
+    }
+
+    //reduce 세번째 방법
+    //세 번째 reduce 메서드는 세 개의 매개변수를 받는다.
+    // 항등값, BiFunction accumulator 그리고 BinaryOperator 타입의 combiner 함수.
+    // 항등값의 타입이 Person 으로 제한되지 않기 때문에 우리는 모든 사람들의 나이의 합을 알아내는데 이 감소연산을 활용할 수 있다.
+    static void test8() {
+        System.out.println("DeepenConcept.test8");
+        Integer ageSum = persons
+                .stream()
+                .reduce(0, (sum, p) -> sum += p.getAge(), (sum1, sum2) -> sum1 + sum2);
+
+        System.out.println(ageSum);
+
+        ageSum = persons.stream()
+                .reduce(0,
+                        (sum,p)->{
+                            System.out.println("sum = " + sum+",person:"+p);;
+                            return sum+=p.getAge();
+                            },
+
+                        (sum1, sum2)->{
+                            System.out.println("sum1 = " + sum1+", sum2="+sum2);
+                            return sum1+sum2;
+                        });
+    }
+
+    static void test9() {
+
+    }
+
+    //map reduce 공부
+    //오늘 이거 보기 http://iloveulhj.github.io/posts/java/java-stream-api.html
+    static void test10() {
+        persons.stream().reduce(0,
+                (sum, p)->{
+                    System.out.println("accum sum:"+ sum+", person:"+p);
+                    return sum += p.getAge();
+                },
+                (sum1, sum2)->{
+                    //이게 출력이 안됨
+                    System.out.println("combiner sum1:"+sum1+", sum2:"+sum2);
+                    return sum1 +sum2;
+                });
+        System.out.println("pararell stream------------------------");
+        persons.parallelStream().reduce(0,
+                (sum, p)->{
+                    System.out.println("accum sum:"+ sum+", person:"+p);
+                    return sum += p.getAge();
+                },
+                (sum1, sum2)->{
+                    //이게 출력이 안됨
+                    System.out.println("combiner sum1:"+sum1+", sum2:"+sum2);
+                    return sum1 +sum2;
+                });
+        //근데 실전에서는 mapToInt를 마니 사용한다.
+        int result = persons.stream().mapToInt(p->p.getAge()).sum();
+        System.out.println("maptoint result:"+result);
     }
 }
